ec61380ce58d2c28408424489a8b294576d40046
vadym <vsukolen@anaqua.com>
2023-08-02 19:48:33 +0800

    add fmt, spdlog as submodules

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,6 @@
 #include "CLI/App.hpp"
 #include "CLI/Config.hpp"
 #include "CLI/Formatter.hpp"
-#include <fmt/core.h>
 #include <simple_cpp_xml/exception.hpp>
 #include <spdlog/spdlog.h>
 
@@ -17,6 +16,19 @@ template<> struct fmt::formatter<CoverageInfo> : fmt::formatter<std::string>
   }
 };
 
+auto readFile(auto path)
+{
+  std::ifstream stream(path);
+  if (stream.is_open()) {
+    std::stringstream buffer;
+    buffer << stream.rdbuf();
+    stream.close();
+    return buffer.str();
+  } else {
+    throw std::runtime_error("Failed to open task input file");
+  }
+}
+
 int main(int argc, const char **argv)
 {
   CLI::App app{ "App description" };
@@ -25,9 +37,11 @@ int main(int argc, const char **argv)
   app.add_option("-j,--jacoco", filename, "Jacoco XML report")->required();
 
   CLI11_PARSE(app, argc, argv);
+
+  const auto content = readFile(filename);
   try {
     JacocoParser parser;
-    parser.parse(filename);
+    parser.parse(content);
     spdlog::info("Parsed coverage: \n{}", parser.getCoverageInfo());
   } catch (const simple_cpp::xml::ParseError &ex) {
     spdlog::error("Failed to parse xml {}", ex.what());
